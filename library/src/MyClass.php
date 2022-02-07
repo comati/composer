484155d8b0f6e4fe03d62bec8a7dc42963c57c36
comati <76742956+comati@users.noreply.github.com>
2022-02-08 01:21:45 +0200

    Add files via upload

---
+++ library/src/MyClass.php
@@ -0,0 +1,11 @@
+<?php
+// The filename should match the class name
+
+namespace DevDungeon;
+
+class MyClass {
+    var $something = 'Some text';
+    public function say_hello() {
+        echo 'Hello!';
+    }
+}
